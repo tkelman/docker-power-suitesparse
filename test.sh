e2db354023af946e2a00599ce866f36d8acfe2eb
Tony Kelman <tony@kelman.net>
2017-03-09 15:27:35 -0800

    building the same code twice, don't need warnings the second time

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 $CC -O2 -o mwe-O2 *.i -fno-diagnostics-show-caret -fexceptions -fPIC -lm /usr/lib/libblas.so.3
 timeout 20 qemu-ppc64le ./mwe-O2
-$CC -O3 -o mwe-O3 *.i -fno-diagnostics-show-caret -fexceptions -fPIC -lm /usr/lib/libblas.so.3
+$CC -O3 -o mwe-O3 *.i -w -fexceptions -fPIC -lm /usr/lib/libblas.so.3
 ! timeout 20 qemu-ppc64le ./mwe-O3
