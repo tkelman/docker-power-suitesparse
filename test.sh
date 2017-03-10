a132248cbb0e333a28603f4ca541438c114f36b9
Tony Kelman <tony@kelman.net>
2017-03-10 04:22:53 -0500

    little bit of async

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 gcc -O2 -o mwe-O2-native *.i $CFLAGS -fexceptions -fPIC -lm /home/test/libopenblas.so.0 -Wl,-rpath=/home/test
 timeout 20 ./mwe-O2-native
-$CC -O2 -o mwe-O2 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3
-timeout 20 qemu-ppc64le ./mwe-O2
-$CC -O3 -o mwe-O3 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3
-! timeout 20 qemu-ppc64le ./mwe-O3
+$CC -O2 -o mwe-O2 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3 &
+$CC -O3 -o mwe-O3 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3 &
+wait %1
+timeout 20 qemu-ppc64le ./mwe-O2 2>/dev/null
+wait %2
+! timeout 20 qemu-ppc64le ./mwe-O3 2>/dev/null
