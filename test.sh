c28b00f372c798c288505445058aba0d237db61a
Tony Kelman <tony@kelman.net>
2017-03-10 06:22:43 -0500

    async probably oversubscribing

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 gcc -O2 -o mwe-O2-native *.i $CFLAGS -fexceptions -fPIC -lm /home/test/libopenblas.so.0 -Wl,-rpath=/home/test
 timeout 20 ./mwe-O2-native
-$CC -O2 -o mwe-O2 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3 &
-$CC -O2 -ftree-slp-vectorize -o mwe-O3 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3 &
-wait %1
+$CC -O2 -o mwe-O2 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3
 timeout 20 qemu-ppc64le ./mwe-O2 2>/dev/null
-wait %2
+$CC -O2 -ftree-slp-vectorize -o mwe-O3 *.i $CFLAGS -fexceptions -fPIC -lm /usr/lib/libblas.so.3
 ! timeout 20 qemu-ppc64le ./mwe-O3 2>/dev/null
